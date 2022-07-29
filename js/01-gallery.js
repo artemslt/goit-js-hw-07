7861df80148bdc21e819466ec45290fcd1d7ea2a
Artem <ppt.artem@gmail.com>
2022-07-29 16:20:25 +0300

    hwc_v1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,50 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryDiv = document.querySelector('.gallery');
+
+galleryDiv.innerHTML = galleryItems
+  .map(
+    item =>
+      `<div class="gallery__item">
+      <a class="gallery__link"
+       href="${item.original}">
+        <img class="gallery__image"
+         src="${item.preview}" 
+         data-source="${item.original}"
+          alt="${item.description}" />
+      </a>
+    </div>`
+  )
+  .join('');
+
+const clickOnImage = evt => {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  const largeImg = evt.target.dataset.source;
+  const instance = basicLightbox.create(
+    `
+    <div class="modal">
+       		<img  width="1280" src=${largeImg} >
+    </div>
+`,
+    {
+      onShow: instance => {
+        instance.element().querySelector('img').onclick = instance.close;
+      },
+    }
+  );
+
+  instance.show();
+
+  document.addEventListener('keydown', evt => {
+    if (evt.key === 'Escape') {
+      return instance.close();
+    }
+  });
+};
+
+galleryDiv.addEventListener('click', clickOnImage);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryDiv = document.querySelector('.gallery');
+
+galleryDiv.innerHTML = galleryItems
+  .map(
+    item =>
+      `<li>
+      <a class="gallery__item"
+       href="${item.original}">
+        <img class="gallery__image"
+         src="${item.preview}" 
+          alt="${item.description}" />
+      </a>
+      </li>
+    `
+  )
+  .join('');
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
